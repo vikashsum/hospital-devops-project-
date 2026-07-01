7c0f99b418ef4d11561e7704ad00faf24db66174
vikash <vikashsuman886@gmail.com>
2026-07-01 17:40:49 +0530

    remove unused jenkins terraform module

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,4 +3,3 @@ output "cluster_endpoint" { value = module.eks.cluster_endpoint }
 output "vpc_id" { value = module.vpc.vpc_id }
 output "ecr_repository_url" { value = module.ecr.repository_url }
 output "alb_dns_name" { value = module.alb.alb_dns_name }
-output "jenkins_public_ip" { value = module.jenkins.public_ip }
